6e89c6608d92fc27c330ac48aee9b08a150708b9
Stephen <whippetlegs@gmail.com>
2013-05-31 21:28:38 +0000

    Overlay completed

--- Week 1/Homework/overlay-starter.rkt
+++ Week 1/Homework/overlay-starter.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname overlay-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-
+(require 2htdp/image)
 ;; overlay-starter.rkt
 
 ; 
@@ -13,3 +13,7 @@
 ; You can consult the DrRacket help desk for information on how to use star and overlay. 
 ; Don't worry about the exact size of the stars.
 ; 
+
+(overlay (star 60 "solid" "blue")
+         (star 100 "solid" "red")
+         (star 140 "solid" "black"))
