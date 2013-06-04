a7ce030dac77a9f0d0c202f9b08b3611011a1fa5
Stephen <whippetlegs@gmail.com>
2013-06-04 21:33:35 +0000

    Week two practise problem starter code added

---
+++ Week 2/Practise problems/ensure-question-starter.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ensure-question-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+;; ensure-question-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; Use the How to Design Functions (HtDF) recipe to design a function that consumes a string, and adds "?" 
+; to the end unless the string already ends in "?".
+; 
+; For this question, assume string is a word, so it has length > 0. Your complete design should include 
+; signature, purpose, commented out stub, examples/tests, commented out template and the completed function 
+; definition.
+; 
---
+++ Week 2/Practise problems/make-box-starter.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname make-box-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(require 2htdp/image)
+
+;; make-box-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; You might want to create boxes of different colours.
+; 
+; Use the How to Design Functions (HtDF) recipe to design a function that consumes a colour, and creates
+; a solid 10x10 box of that colour.
+; 
+; Your complete design should include signature, purpose, commented out stub, examples/tests, 
+; commented out template and the completed function definition.
+
