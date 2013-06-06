0bd21d021781e9c41b60c23d4f772e04673adf1a
Stephen <whippetlegs@gmail.com>
2013-06-06 20:26:23 +0000

    Started first lecture problem

--- Week 2/Lecture problems/double-starter.rkt
+++ Week 2/Lecture problems/double-starter.rkt
@@ -12,3 +12,12 @@
 ; template.
 ; 
 
+;; Number -> Number
+;; produce 2 times the given number
+(check-expect (double 3) 6)
+(check-expect (double 4.2) 8.4)
+
+;(define (double n) 0) ;this is the stub
+
+;(define (double n)    ;this is the template
+
