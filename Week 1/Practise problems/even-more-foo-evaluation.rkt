68147449413f0b5a2c52aec679cfd32c2a55bb6f
Stephen <whippetlegs@gmail.com>
2013-06-09 09:43:54 +0000

    Initial commit

--- Week 1/Practise problems/even-more-foo-evaluation.rkt
+++ Week 1/Practise problems/even-more-foo-evaluation.rkt
@@ -8,12 +8,13 @@
 ; 
 ; Given the following function definition:
 ; 
-; (define (farfle s)
-;   (string-append s s))
+ (define (farfle s)
+   (string-append s s))
 ; 
 ; Write out the step-by-step evaluation of the expression: 
 ; 
-; (farfle (substring "abcdef" 0 2))
+ (farfle (substring "abcdef" 0 2))
 ; 
 ; Be sure to show every intermediate evaluation step.
 ; 
+
