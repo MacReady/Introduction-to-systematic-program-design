428ff39227c8fa1a71cf103927535f94cd8d1ac2
Stephen <whippetlegs@gmail.com>
2013-06-09 09:32:01 +0000

    First attempt at more foo evaluation

--- Week 1/Practise problems/more-foo-evaluation.rkt
+++ Week 1/Practise problems/more-foo-evaluation.rkt
@@ -8,8 +8,8 @@
 ; 
 ; Given the following function definition:
 ; 
-; (define (foo n)
-;   (* n n))
+ (define (foo n)
+   (* n n))
 ; 
 ; Write out the step-by-step evaluation of the expression: 
 ; 
@@ -17,3 +17,6 @@
 ; 
 ; Be sure to show every intermediate evaluation step.
 ; 
+
+(foo 7)
+(* 7 7)
