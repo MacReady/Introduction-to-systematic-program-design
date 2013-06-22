4c1c1be113b3c3a05bf63a8a0312894e33c4b1b2
Stephen <whippetlegs@gmail.com>
2013-06-22 15:18:42 +0000

    First data definition example

---
+++ Week 3/Lecture problems/city-name.rkt
@@ -0,0 +1,25 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname city-name) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+;; city-name-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; Imagine that you are designing a program that, among other things, 
+; has information about the names of cities in its problem domain.
+; 
+; Design a data definition to represent the name of a city. 
+;    
+
+;; CityName is String
+;; interp. the name of a city
+(define CN1 "Reykjavik")
+(define CN2 "Isafjordur")
+#;
+(define (fn-for-city-name cn)
+  (... cn))
+
+;; Template rules used:
+;; - atomic non-distinct: String
