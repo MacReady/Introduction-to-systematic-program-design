4df1e30f9fcb10fd1b8247c536d862dc46860ce6
Stephen <whippetlegs@gmail.com>
2013-06-23 20:23:32 +0000

    Dinner problem finished

--- Week 3/Homework/dinner.rkt
+++ Week 3/Homework/dinner.rkt
@@ -19,7 +19,25 @@
 ; Call the type DinnerOrder.
 ; 
 
+;;DinnerOrder is one of:
+;; - "chicken"
+;; - "pasta"
+;; - "no meal order"
+;; interp. the type of meal ordered
 
+;;<examples are redundant for enumerations>
+
+#;
+(define (fn-for-dinner-order do)
+  (cond [(string? "chicken" do) (...)]
+        [(string? "pasta" do) (...)]
+        [(string? "no meal order") (...)]))
+
+;; Template rules used:
+;; - one of: 3 cases
+;; - atomic distinct: "chicken"
+;; - atomic distinct: "pasta"
+;; - atomic distinct: "no meal order"
 ;; =================
 ;; Functions:
 
@@ -32,5 +50,21 @@
 ; For example, calling dinner-order-to-msg for a chicken dinner 
 ; would produce "The passenger ordered chicken."
 ; 
+
+;; String -> String
+;; produces messge depending on what is ordered
+(check-expect (dinner-order-to-msg "chicken") "winner winner chicken dinner")
+(check-expect(dinner-order-to-msg "pasta") "pasta pronto")
+(check-expect (dinner-order-to-msg "no meal order") "no soup for you")
+
+;(define (dinner-order-to-msg s) "") ;stub
+
+;<use template from DinnerOrder>
+
+(define (dinner-order-to-msg s)
+  (cond [(string=? s "chicken") "winner winner chicken dinner"]
+        [(string=? s "pasta") "pasta pronto"]
+        [(string=? s "no meal order") "no soup for you"]))
+
 
 
