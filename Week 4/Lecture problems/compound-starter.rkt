f834b0e445d3e2c7a2ad65006d3411c96e677563
Stephen <whippetlegs@gmail.com>
2013-06-27 09:51:22 +0000

    Compound starter finished

--- Week 4/Lecture problems/compound-starter.rkt
+++ Week 4/Lecture problems/compound-starter.rkt
@@ -11,3 +11,17 @@
 ; their first and last names.
 ; 
 
+(define-struct player (fn ln))
+;; Player is (make-player String String)
+;; interp. (make-player fn ln) is a hockey player with
+;;          fn is the first name
+;;          ln is the last name
+(define P1 (make-player "Bobby" "Orr"))
+(define P2 (make-player "Wayne" "Gretsky"))
+
+(define (fn-for-player p)
+  (... (player-fn P)      ;String
+       (player-ln p)))    ;String
+
+;; Template rules used:
+;; - Compound: 2 fields
