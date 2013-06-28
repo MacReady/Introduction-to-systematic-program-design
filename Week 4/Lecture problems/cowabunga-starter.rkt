7f4017e15b3ec24f4f5690c7597028070c54e241
Stephen <whippetlegs@gmail.com>
2013-06-28 20:39:09 +0000

    Removed embedded images from file

--- Week 4/Lecture problems/cowabunga-starter.rkt
+++ Week 4/Lecture problems/cowabunga-starter.rkt
@@ -1,7 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname cowabunga-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-
+(require 2htdp/image)
+(require 2htdp/universe)
 ; 
 ; PROBLEM:
 ; 
@@ -30,5 +31,81 @@
 ; Also, to make it look better, arrange for the cow to change direction when its
 ; nose hits the edge of the window, not the center of its body.
 ; 
+
+;; Constants:
+
+(define WIDTH 400)
+(define HEIGHT 200)
+
+(define CTR-Y (/ HEIGHT 2))
+
+(define RCOW )
+
+(define LCOW )
+
+(define MTS (empty-scene WIDTH HEIGHT))
+
+;; Data definitions:
+(define-struct cow (x dx))
+;; Cow is (make-cow Natural[0, WIDTH], Integer)
+;; interp. (make-cow x dx) is a cow with x coordinate x and velocity dx
+;;         the x is the center of the cow
+;;         x is in screen coordinates (pixels)
+;;         dx is in pixels per tick
+;;
+(define C1 (make-cow 10 3))  ; at 10, moving left -> right
+(define C2 (make-cow 20 -4)) ; at 20, moving left <- right
+#;
+(define (fn-for-cow c)
+  (... (cow-x c)      ;Natural[0, WIDTH]
+       (cow-dx c)))   ;Integer
+
+;; Template rules used:
+;; - compound: 2 fields
+
+;; Functions:
+
+;; Cow -> Cow
+;; called to make the cow go for a walk; start with (make-cow 0 3))
+;; no tests for main function
+(define (main c)
+  (big-bang c
+            (on-tick next-cow)      ; Cow -> Cow
+            (to-draw render-cow)    ; Cow -> Image
+            (on-key handle-key)))   ; Cow KeyEvent -> Cow 
+
+;; Cow -> Cow
+;; increase cow x by dx; bounce off edges
+(check-expect (next-cow (make-cow 20 3)) (make-cow (+ 20 3) 3))  ;middle
+(check-expect (next-cow (make-cow 20 -3)) (make-cow (- 20 3) -3))
+              
+(check-expect (next-cow (make-cow (- WIDTH 3) 3)) (make-cow WIDTH 3))  ;reaches edge
+(check-expect (next-cow (make-cow 3 -3)) (make-cow 0 -3))
+
+(check-expect (next-cow (make-cow (- WIDTH 2) 3)) (make-cow WIDTH -3)) ;tries to pass edge
+(check-expect (next-cow (make-cow 2 -3)) (make-cow 0 3))
+
+
+;    ;stub
+
+;took template from Cow
+(define (next-cow c)
+  (cond [(... (cow-x c)      
+       (cow-dx c)))   
+
+;; Cow -> Image
+;; place appropriate cow image on MTS at (cow-x c) and CTR-Y
+(define (render-cow c) MTS)  ;stub
+
+;; Cow KeyEvent -> Cow
+;; reverse direction of cow travel when space bar is pressed
+;; !!!
+(define (handle-key c ke) c) ;stub
+
+
+
+
+
+
 
 
